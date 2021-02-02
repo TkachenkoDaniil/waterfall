474e2f9827f947775af9a77c26e3561a5161aef1
daniil <dtkachenko41@gmail.com>
2021-02-02 22:29:59 +0300

    created first waterfall

---
+++ src/visual.ts
@@ -0,0 +1,325 @@
+"use strict";
+
+import "core-js/stable";
+import "./../style/visual.less";
+import powerbi from "powerbi-visuals-api";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualObjectInstance = powerbi.VisualObjectInstance;
+import DataView = powerbi.DataView;
+import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
+import ISelectionManager = powerbi.extensibility.ISelectionManager;
+
+import { VisualSettings } from "./settings";
+import * as d3 from "d3";
+
+interface DataPoint{
+    category: string;
+    value: number;
+    color: string;
+    identity: powerbi.visuals.ISelectionId;
+    highlighted: boolean;
+    marginBottom: number;
+}
+
+interface ViewModel{
+    dataPoints: DataPoint[];
+    maxValue: number;
+    highlights: boolean;
+}
+
+export class Visual implements IVisual {
+
+    private host: IVisualHost;
+    private svg: d3.Selection<SVGElement>;
+    private barGroup: d3.Selection<SVGElement>;
+    private xPadding: number = 0.1;
+    private selectionManager: ISelectionManager;
+    private xAxisGroup: d3.Selection<SVGElement>;
+    private yAxisGroup: d3.Selection<SVGElement>;
+
+    private settings = {
+        axis : {
+            x : {
+                padding: {
+                    default: 50,
+                    value: 50
+                },
+                show: {
+                    default: true,
+                    value: true
+                }
+            },
+            y : {
+                padding: {
+                    default: 50,
+                    value: 50
+                }
+            }
+        },
+        border : {
+            top: {
+                default: 0,
+                value: 0
+            }
+        }
+    }
+
+    constructor(options: VisualConstructorOptions) {
+        this.host = options.host;
+        this.svg = d3.select(options.element)
+            .append("svg")
+            .classed("my-little-bar-chart", true);
+        this.barGroup = this.svg.append("g")
+            .classed("bar-group", true);
+
+        this.xAxisGroup = this.svg.append("g")
+            .classed("x-axis", true);
+        
+        this.yAxisGroup = this.svg.append("g")
+            .classed("y-axis", true);
+
+        this.selectionManager = this.host.createSelectionManager();
+        }
+
+    public update(options: VisualUpdateOptions) {
+//debugger;
+        this.updateSettings(options);
+        let viewModel: ViewModel = this.getViewModel(options);
+        viewModel = this.calculateMarginsOfColumns(viewModel);
+
+        let width = options.viewport.width;
+        let height = options.viewport.height;
+
+        let xAxisPadding = this.settings.axis.x.show.value ? this.settings.axis.x.padding.value : 0;
+
+        this.svg.attr({
+            width: width,
+            height: height,
+        });
+//debugger;
+        //let a = this.settings.axes.x.padding;
+        let yScale = d3.scale.linear()
+            .domain([0, viewModel.maxValue])
+            .range([height - xAxisPadding, 0 + this.settings.border.top.value]);
+            // .range([0 + this.settings.border.top.value, height - xAxisPadding]);
+
+        let xScale = d3.scale.ordinal()
+            .domain(viewModel.dataPoints.map(d => d.category))
+            .rangeRoundBands([this.settings.axis.x.padding.value, width], this.xPadding);
+
+        let xAxis = d3.svg.axis()
+            .scale(xScale)
+            .orient("bottom")
+            .tickSize(1);
+        
+        let yAxis = d3.svg.axis()
+            .scale(yScale)
+            .orient("left")
+            .tickSize(0.5);
+        
+        this.yAxisGroup
+        .call(yAxis)
+        .attr({
+            transform: "translate(" + this.settings.axis.y.padding.value + ",0)"
+            // ("transform","\"translate(0, " + (height - this.settings.axes.x.padding) + ")\"")
+        })
+        .style({
+            fill: "#777777"
+        })
+        .selectAll("text")
+        // .attr({
+        //     transform: "rotate(-35)"
+        // })
+        .style({
+            "text-anchor": "end",
+            "font-size": "x-small"
+        });
+        console.log('asdafasf ', yScale(viewModel.maxValue))
+
+        //xAxis(this.xAxisGroup);
+        this.xAxisGroup
+            .call(xAxis)
+            .attr("transform", "translate(0, " + (height - xAxisPadding) + ")")
+            
+            //("transform","\"translate(0, " + (height - this.settings.axes.x.padding) + ")\"")
+            
+            .style({
+                fill: "#777777"
+            })
+            .selectAll("text")
+            .attr({
+                transform: "rotate(-35)"
+            })
+            .style("text-anchor", "end")
+            .style("font-size", "x-small");
+
+        let bars = this.barGroup
+            .selectAll(".bar")
+            .data(viewModel.dataPoints);
+
+        bars.enter()
+            .append("rect")
+            .classed("bar", true);
+
+        bars
+            .attr("width", xScale.rangeBand())
+            .attr("height", (d) => height - yScale(d.value) - xAxisPadding)
+            .attr("y", d => {
+              console.log('scale', yScale(d.value));
+              return yScale(d.value) - yScale(viewModel.maxValue - d.marginBottom)
+            //   return yScale(d.value)
+            })
+            .attr("x", d => xScale(d.category))
+
+            .style("fill", d => d.color)
+            .style("fill-opacity", d => viewModel.highlights ? d.highlighted ? 1.0 : 0.5 : 1.0)
+
+            .on("click", d => {
+                this.selectionManager
+                    .select(d.identity, true)
+                    .then(ids => {
+                        bars.style({
+                            "fill-opacity": ids.length > 0 ? 
+                                d => ids.indexOf(d.identity) >= 0 ? 1.0 : 0.5
+                                : 1.0
+                        });
+                    });
+            });
+
+        bars.exit()
+            .remove();
+    }
+
+    private calculateMaxValue (viewModel: ViewModel): number {
+      // console.log(viewModel);
+      // console.log(this.settings);
+      const maxValue = viewModel.dataPoints.reduce(
+        (accum, currentValue) => {
+          // const { value } = currentValue;
+          return accum + currentValue.value
+        }, 0);
+      console.log(maxValue);
+      return maxValue;
+    };
+
+    private calculateMarginsOfColumns(viewModel: ViewModel): ViewModel {
+      console.log('viewModel', viewModel)
+    //   debugger;
+      const transformedViewModel = viewModel.dataPoints.reduce(
+        (accum, currentValue, index, array) => {
+        //   const { value } = currentValue;
+        // let value: number, marginBottom: number;
+        // if (index !== 0) {
+        //     value = accum[accum.length-1].value;
+        //     marginBottom = accum[accum.length-1].marginBottom
+        //     // ({ value, marginBottom } = accum[accum.length]);
+        // } else {
+        //     value = 0;
+        //     marginBottom = 0;
+        // }
+        let { value, marginBottom } = index !== 0 ? accum[accum.length-1] : { value: 0, marginBottom: 0 };
+        // const { value, marginBottom } =  { value: 0, marginBottom: 0 }
+        //   const acc = value + marginBottom;
+            //   const nextValue = value + prevValue;
+          accum.push({
+            ...currentValue,
+            // marginBottom: value + prevValue,
+            marginBottom: value + marginBottom,
+            // value: 0,
+          })
+          return accum
+        }, [],
+      );
+      console.log('transformedViewModel', transformedViewModel)
+      viewModel.dataPoints = transformedViewModel;
+      viewModel.maxValue = this.calculateMaxValue(viewModel);
+      return viewModel;
+    };
+
+    private updateSettings(option: VisualUpdateOptions) {
+        let dv = option.dataViews;
+
+        if( !dv
+            || !dv[0]
+            || !dv[0].metadata
+            || !dv[0].metadata.objects
+            || !dv[0].metadata.objects.xAxis)
+            return null;
+        
+        let metadata = dv[0].metadata;
+        let objects = metadata.objects;
+        let xAxis = objects.xAxis;
+        this.settings.axis.x.show.value = <boolean>xAxis.show;
+    }
+
+    private getViewModel(options: VisualUpdateOptions): ViewModel {
+//debugger;
+        let dv = options.dataViews;
+        let viewModel: ViewModel = {
+            dataPoints: [],
+            maxValue: 0,
+            highlights: false
+        };
+
+        if (!dv
+            || !dv[0]
+            || !dv[0].categorical.categories
+            || !dv[0].categorical.categories[0].source
+            || !dv[0].categorical.values
+            || !dv[0].categorical.values)
+            return viewModel;
+
+        let view = dv[0].categorical;
+        let categories = view.categories[0];
+        let values = view.values[0];
+        let highlights = values.highlights;
+
+        for (let i = 0, len = Math.max(categories.values.length, values.values.length); i < len; i++) {
+            viewModel.dataPoints.push({
+                category: <string>categories.values[i],
+                value: <number>values.values[i],
+                color: this.host.colorPalette.getColor(<string>categories.values[i]).value,
+                identity: this.host.createSelectionIdBuilder()
+                    .withCategory(categories, i)
+                    .createSelectionId(),
+                highlighted: highlights ? highlights[i] ? true : false : false,
+                marginBottom: 0,
+            });
+            //console.log(viewModel.dataPoints[i].identity)
+        }
+
+        // viewModel.maxValue = d3.max(viewModel.dataPoints, d => d.value)
+        viewModel.maxValue = this.calculateMaxValue(viewModel);
+        console.log('dataPoints', viewModel.dataPoints);
+        viewModel.highlights = viewModel.dataPoints.filter(d => d.highlighted).length > 0;
+        // viewModel.marginBottom = 0;
+//debugger;
+
+        return viewModel;
+    }
+
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
+        //const settings: VisualSettings = this.visualSettings || <VisualSettings>VisualSettings.getDefault();
+        //return VisualSettings.enumerateObjectInstances(settings, options);
+        let propertyGroupName = options.objectName;
+        let properties: VisualObjectInstance[] = [];
+
+        switch (propertyGroupName) {
+            case "xAxis":
+                properties.push({
+                    objectName: propertyGroupName,
+                    properties: {
+                        show: this.settings.axis.x.show.value
+                    },
+                    selector: null
+                });
+                break;
+        };
+        return properties;
+    }
+}
